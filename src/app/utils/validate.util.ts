4cfe97f89b56eb939fde793f8756843adc204e32
mahmoud fady <mahmoudfady02@gmail.com>
2023-07-06 12:55:06 +0300

    optimize string valid func (util)

--- src/app/utils/validate.util.ts
+++ src/app/utils/validate.util.ts
@@ -7,14 +7,11 @@ const validateString = (
     max: 10,
     require: true,
     regex: new RegExp(/^[A-Za-z]+(?:[-\s][A-Za-z]+)*$/),
+    ...target,
   };
-  for (let k in target) {
-    criteria[k] = target[k];
-  }
-  console.log(val);
   if (val.length < criteria.min) throw "filed min length " + criteria.min;
-  if (val.length > criteria.max) throw "filed max length " + criteria.max;
+  else if (val.length > criteria.max) throw "filed max length " + criteria.max;
   else if (!criteria.regex.test(val)) throw "filed is invalid";
-  return val;
+  else return val;
 };
 export { validateString };
