44fc4a4530a92fa03d091585b27bc761d1b8f81d
Mahmoud Fady <mahmoudfady02@gmail.com>
2023-07-21 14:55:52 +0300

    add mongoose sanitize mw

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,16 @@
 import express from "express";
 import cors from "cors";
 import path from "path";
+import mongoSanitize from "express-mongo-sanitize";
 import userRoutes from "./routes/user.routes";
 import postRoutes from "./routes/post.routes";
 import notFoundMw from "./middlewares/not-found.mw";
 import errorMw from "./middlewares/error.mw";
-import loggingMw from "./middlewares/logging.mw";
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+app.use(mongoSanitize());
 app.use("/uploads", express.static(path.join(__dirname, "../", "uploads")));
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/posts", postRoutes);
